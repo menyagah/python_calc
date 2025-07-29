3dd4bf3378fd9d7cc9db0e222cfd3145d949c304
menyagah <menyagah27@gmail.com>
2025-07-29 08:05:47 +0300

    Create python program to take user input and perform calculations

---
+++ calc.py
@@ -0,0 +1,40 @@
+def calc():
+    while True:
+        user_input = input("please input two numbers and an arithmetic separated by a comma, +, -, /, * or 'q' to quit: ")
+        if user_input == 'q':
+            print("Exiting caculator program...")
+            break
+        arithmeic_values = ['*', '+', '/','-']
+        user_input = user_input.split(',')
+        removed_item = None
+        for item in arithmeic_values:
+            if item in user_input:
+                user_input.remove(item)
+                removed_item = item
+                break
+        if not removed_item:
+            print("please input a valid arithmetic x, +, -, or /")
+        
+        try:
+            user_input = [int(i) for i in user_input]
+        except ValueError as e:
+            print(f"Invalid input: all operands must be numbers! Details: {e}")
+        a, b = user_input
+        if len(user_input) != 2:
+            print('Please provide exactly two numbers')
+        match removed_item:
+            case '+':
+                print(a + b)
+            case '*':
+                print(a * b)
+            case '-':
+                print(a - b)
+            case '/':
+                if b == 0:
+                    print("cannot divide by zero")
+                print(a / b)
+        
+
+  
+              
+   
