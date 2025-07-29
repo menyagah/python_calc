58be572b782bd2b7b30ca9eb7d2a7a3daa8a59e0
menyagah <menyagah27@gmail.com>
2025-07-29 08:39:59 +0300

    make errors more user friendly and print the solution using the operand also

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 def calc():
     while True:
-        user_input = input("please input two numbers and an arithmetic separated by a comma, +, -, /, * or 'q' to quit: ")
+        user_input = input("\n Please input two numbers and an arithmetic separated by a comma, +, -, /, * or 'q' to quit: ")
         if user_input == 'q':
             print("Exiting caculator program...")
             break
@@ -13,7 +13,7 @@ def calc():
                 removed_item = item
                 break
         if not removed_item:
-            print("please input a valid arithmetic x, +, -, or /")
+            print("please input a valid arithmetic *, +, -, or /")
         
         try:
             user_input = [int(i) for i in user_input]
@@ -21,20 +21,20 @@ def calc():
             print(f"Invalid input: all operands must be numbers and a python arithmetic operator! Details: {e}")
         
         if len(user_input) != 2:
-            print('Please provide exactly two numbers')
+            print('Please provide exactly two numbers, separated by a comma! \n e.g. 3,2,+ \n')
         else:
             a, b = user_input
             match removed_item:
                 case '+':
-                    print(a + b)
+                    print(f"The solution of {a} + {b} is {a + b}")
                 case '*':
-                    print(a * b)
+                    print(f"The solution of {a} * {b} is {a * b}")
                 case '-':
-                    print(a - b)
+                    print(f"The solution of {a} - {b} is {a - b}")
                 case '/':
                     if b == 0:
                         print("cannot divide by zero")
-                    print(a / b)
+                    print(f"The solution of {a} / {b} is {a / b}")
         
 
 calc()  
